2170a22241b9769da9b5092eb141e98fa061c5a5
Brice <bricecoulibaly596@gmail.com>
2022-04-19 18:24:46 +0000

    front-end-base-de-donné

---
+++ css/formulaire.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/formulaire.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="container">
+                <div class="row">
+                    <div class="col-4">
+                        <img src="images/logo.jpg" alt="logo-auf">
+                    </div>
+                    <div class="col-3">
+                        <a href="">LISTE DES ABONNES</a>
+                    </div>
+                    <div class="col-3">
+                        <a href="">DECONEXION</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
